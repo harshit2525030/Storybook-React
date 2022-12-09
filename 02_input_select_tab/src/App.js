94aa7823b8626894bc3c94d500ca70905db0b65c
harshit2525030 <115477965+harshit2525030@users.noreply.github.com>
2022-12-09 16:44:38 +0530

    Add files via upload

---
+++ 02_input_select_tab/src/App.js
@@ -0,0 +1,17 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Button from "./components/Button";
+
+function App() {
+  return (
+    <> </>
+    // <Router>
+    //   <div className="App">
+    //     <Routes>
+    //       <Route path="/" element={<Button />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
+  );
+}
+
+export default App;
